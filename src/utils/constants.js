88af4a67903b8e7ef33e44a607526511574117fc
Mayank Raj Parmar <mayankrajparmar@gmail.com>
2025-03-02 19:19:26 +0530

    changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
 export const LINKEDIN_URL = "https://www.linkedin.com/in/mayankrajparmar/";
 
 export const RESTAURANT_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const RESTAURANT_MENU_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9953514&lng=77.65883749999999&restaurantId=";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9953514&lng=77.65883749999999&restaurantId=";
